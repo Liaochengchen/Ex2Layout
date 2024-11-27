994a2b7bbd9d813cd3ff07ab5ac131e8a373ef4c
lcc <1223282080@qq.com>
2024-11-27 15:58:36 +0800

    2

---
+++ constraintlayout1/src/main/java/com/example/constraintlayout1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.constraintlayout1;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.constraintlayout1.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
